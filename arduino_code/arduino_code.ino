cb448f981cb08229914b8dee5a341805f2696edc
truongbilly <123608561+truongbilly@users.noreply.github.com>
2023-01-31 19:43:31 -0500

    added test cases

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -4,14 +4,21 @@ int y = 150;
 void setup() {
 	Serial.begin(115200); // use the same baud-rate as the python side
 
-  delay(5);
+  delay(10);
 
+  // Test highlighting
+  // for (int i = 0; i < 100; i++) {
+  //   x++;
+  //   char buffer[10];
+  //   sprintf(buffer, "%d, %d", x, y);
+  //   Serial.println(buffer);
+  // }
+
+  // Test button pressing
   for (int i = 0; i < 100; i++) {
-    x++;
     char buffer[10];
-    sprintf(buffer, "%d, %d", x, y);
+    sprintf(buffer, "%d, %d", 7, 2);
     Serial.println(buffer);
-    delay(100);
   }
 }
 void loop() {
