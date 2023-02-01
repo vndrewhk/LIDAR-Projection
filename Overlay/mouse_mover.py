4c419492c04648711f26b2d1a62593e9dc3619da
truongbilly <123608561+truongbilly@users.noreply.github.com>
2023-01-31 19:44:20 -0500

    fixed logic errors with highlighting feature

--- Overlay/mouse_mover.py
+++ Overlay/mouse_mover.py
@@ -1,5 +1,6 @@
 import serial
 import pyautogui
+import time
 
 serial_comm = serial.Serial('COM3', 115200, timeout = 1)
 
@@ -14,10 +15,14 @@ projector_resolution = [400, 300]
 x_conv = (computer_resolution[0]/projector_resolution[0])
 y_conv = (computer_resolution[1]/projector_resolution[1])
 
+time.sleep(5)
+
 while True:
     if serial_comm.in_waiting:
         packet = serial_comm.readline()
         coordinate_string = packet.decode("utf-8").strip('\n') # Receive coordinate as a string
         coordinate_array = list(map(int, coordinate_string.split(", "))) # Convert string to an array [x, y]
         print(coordinate_array)
-        pyautogui.moveTo(coordinate_array[0]*x_conv, coordinate_array[1]*y_conv, duration=0.0, tween=None, logScreenshot=False, _pause=False)
+        pyautogui.mouseDown(button='left', x=coordinate_array[0]*x_conv, y=coordinate_array[1]*y_conv, duration=0.0, tween=None, logScreenshot=False, _pause=False)
+    else:
+        pyautogui.mouseUp() # If there's no data waiting in the serial port, lift the left mouse button up
