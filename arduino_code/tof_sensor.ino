c9b5bcd90ddf749b4e13d38c39b1623d85a2abdf
vuongsteven <stevenvuong2000@gmail.com>
2023-02-05 17:11:15 -0500

    TOF Arduino

---
+++ arduino_code/tof_sensor.ino
@@ -0,0 +1,59 @@
+#include "Adafruit_VL53L1X.h"
+
+#define IRQ_PIN 2
+#define XSHUT_PIN 3
+
+Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
+
+/**
+* VIN - 3.3 V
+* GND - GND
+* SDA - A4
+* SCL - A5
+*/
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  Wire.begin();
+  if (! vl53.begin(0x29, &Wire)) {
+    Serial.print(F("Error on init of VL sensor: "));
+    Serial.println(vl53.vl_status);
+    while (1)       delay(10);
+  }
+  Serial.println(F("VL53L1X sensor OK!"));
+
+  Serial.print(F("Sensor ID: 0x"));
+  Serial.println(vl53.sensorID(), HEX);
+
+  if (! vl53.startRanging()) {
+    Serial.print(F("Couldn't start ranging: "));
+    Serial.println(vl53.vl_status);
+    while (1)       delay(10);
+  }
+  Serial.println(F("Ranging started"));
+
+  vl53.setTimingBudget(50);
+  Serial.print(F("Timing budget (ms): "));
+  Serial.println(vl53.getTimingBudget());
+
+}
+
+void loop() {
+  int16_t distance;
+
+  if (vl53.dataReady()) {
+    distance = vl53.distance();
+    if (distance == -1) {
+      Serial.print(F("Couldn't get distance: "));
+      Serial.println(vl53.vl_status);
+      return;
+    }
+    Serial.print(F("Distance: "));
+    Serial.print(distance);
+    Serial.println(" mm");
+
+    vl53.clearInterrupt();
+  }
+}
